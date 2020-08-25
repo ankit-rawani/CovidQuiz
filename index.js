5c5295dafd7eb48cf1e11e799aa026f91281de19
Ankit Rawani <ankit.rawani.19102001@gmail.com>
2020-08-25 23:28:34 +0530

    Ques navigation functionality added

--- index.js
+++ index.js
@@ -16,16 +16,16 @@ function main(){
     const notifierClose = document.querySelector('#notifier-close')
     const notifierContent = document.querySelector('#notifier-content')
     const questionCount = document.querySelector('#question-count')
+    const questionNumbers = document.querySelectorAll('.qn_number')
+    const rightArrow = document.querySelector('.right-arrow')
+    const leftArrow = document.querySelector('.left-arrow')
 
     let NUMBER_OF_QUES = questions.length
     let NAV_PANE_OPEN = false
     let CURRENT_QUES = 1
     let score
     let IS_ANS_CORRECT = false
-
-    questions.sort(() => {
-        return Math.random() - 0.5
-    })
+    let PLAYER_NAME 
 
     window.onload = evt => {
         console.log(document.body.offsetHeight, window.innerHeight)
@@ -38,9 +38,15 @@ function main(){
             qn.className = 'qn_number'
             qn.id = 'qn_'+i
             qn.textContent = i+1
+            qn.addEventListener('click', () => {
+                CURRENT_QUES = i
+                addQuestion()
+            })
             navPane.appendChild(qn)
         }
 
+        PLAYER_NAME = prompt('Before we begin the quiz, would you like to tell us your name?')
+        if(!PLAYER_NAME) PLAYER_NAME = 'Anonymous'
         showHome()
     }
 
@@ -76,24 +82,24 @@ function main(){
         notifier.style.marginTop = document.querySelector('nav').offsetHeight + 'px'
         notifierOpen = true
 
-        if(evt.target === homeButton) {
-            hideNotifierContent()
-            showHome()
+        if(evt){
+            if(evt.target === homeButton) {
+                hideNotifierContent()
+                showHome()
+            }
+
+            else if (evt.target === instructionsButton) {
+                hideNotifierContent()
+                showInstructions()
+            }
+
+            else if (evt.target === aboutButton) {
+                hideNotifierContent()
+                showAbout()
+            }
+
         }
 
-        else if (evt.target === instructionsButton) {
-            hideNotifierContent()
-            showInstructions()
-        }
-
-        else if (evt.target === aboutButton) {
-            hideNotifierContent()
-            showAbout()
-        }
-
-        else {
-
-        }
     }
 
     function closeNotifier() {
@@ -104,7 +110,7 @@ function main(){
 
     function showHome() {
         let home = document.createElement('div')
-        home.innerHTML = "<h1 class='title'>COVID QUIZ</h1><div class='game-start button'>Start game</div>"
+        home.innerHTML = "<h1 class='title'>COVID QUIZ</h1><div class='about'>Welcome " + PLAYER_NAME +"! Nice to see you here. Just click on the button below to start with the quiz.</div><div class='game-start button'>Start game</div>"
         notifierContent.appendChild(home)
 
         document.querySelector('.game-start').addEventListener('click', startQuiz)
@@ -154,10 +160,61 @@ function main(){
         option3.textContent = questions[CURRENT_QUES].options[2]
         option4.textContent = questions[CURRENT_QUES].options[3]
         questionCount.textContent = CURRENT_QUES+1+'/15'
-        CURRENT_QUES++
 
         removeListenersFromOptions()
-        addListenersToOptions()
+
+        if(questions[CURRENT_QUES].userInput === ''){
+            addListenersToOptions()
+        }
+        else {
+            if (questions[CURRENT_QUES].userInput === questions[CURRENT_QUES].answer){
+                switch (questions[CURRENT_QUES].userInput) {
+                    case 0:
+                        option1.style.borderLeft = '4px solid #22ff55'
+                        break;
+                    case 1:
+                        option2.style.borderLeft = '4px solid #22ff55'
+                        break;
+                    case 2:
+                        option3.style.borderLeft = '4px solid #22ff55'
+                        break;
+                    case 3:
+                        option4.style.borderLeft = '4px solid #22ff55'
+                        break;
+                }
+            }
+
+            else {
+                switch (questions[CURRENT_QUES].answer) {
+                    case 0:
+                        option1.style.borderLeft = '4px solid #22ff55'
+                        break;
+                    case 1:
+                        option2.style.borderLeft = '4px solid #22ff55'
+                        break;
+                    case 2:
+                        option3.style.borderLeft = '4px solid #22ff55'
+                        break;
+                    case 3:
+                        option4.style.borderLeft = '4px solid #22ff55'
+                        break;
+                }
+                switch (questions[CURRENT_QUES].userInput) {
+                    case 0:
+                        option1.style.borderLeft = '4px solid #ff1111'
+                        break;
+                    case 1:
+                        option2.style.borderLeft = '4px solid #ff1111'
+                        break;
+                    case 2:
+                        option3.style.borderLeft = '4px solid #ff1111'
+                        break;
+                    case 3:
+                        option4.style.borderLeft = '4px solid #ff1111'
+                        break;
+                }
+            }
+        }
     }
 
     function addListenersToOptions() {
@@ -175,42 +232,45 @@ function main(){
     }
 
     function nextQues() {
+        CURRENT_QUES++
         addQuestion()
         addListenersToOptions()
         closeNotifier()
     }
 
     function checkAnswer(evt) {
-        if (questions[CURRENT_QUES-1].options[questions[CURRENT_QUES-1].answer] === evt.target.textContent){
+        if (questions[CURRENT_QUES].options[questions[CURRENT_QUES].answer] === evt.target.textContent){
             evt.target.style.borderLeft = '4px solid #22ff55'
             score++
             IS_ANS_CORRECT = true
+            document.querySelector('#qn_' + CURRENT_QUES).style.backgroundColor = '#22ff55'
         }
         else {
             IS_ANS_CORRECT = false
+            document.querySelector('#qn_' + CURRENT_QUES).style.backgroundColor = '#ff1111'
             evt.target.style.borderLeft = '4px solid #ff1111'
 
-            if (questions[CURRENT_QUES - 1].answer == 0){
+            if (questions[CURRENT_QUES].answer == 0){
                 option1.style.borderLeft = '4px solid #22ff55'
             }
 
-            else if (questions[CURRENT_QUES - 1].answer == 1) {
+            else if (questions[CURRENT_QUES].answer == 1) {
                 option2.style.borderLeft = '4px solid #22ff55'
             }
 
-            else if (questions[CURRENT_QUES - 1].answer == 2) {
+            else if (questions[CURRENT_QUES].answer == 2) {
                 option3.style.borderLeft = '4px solid #22ff55'
             }
 
-            else if (questions[CURRENT_QUES - 1].answer == 3) {
+            else if (questions[CURRENT_QUES].answer == 3) {
                 option4.style.borderLeft = '4px solid #22ff55'
             }
         }
 
-        if (evt.target === option1) questions[CURRENT_QUES - 1].userInput = 0
-        if (evt.target === option2) questions[CURRENT_QUES - 1].userInput = 1
-        if (evt.target === option3) questions[CURRENT_QUES - 1].userInput = 2
-        if (evt.target === option4) questions[CURRENT_QUES - 1].userInput = 3
+        if (evt.target === option1) questions[CURRENT_QUES].userInput = 0
+        if (evt.target === option2) questions[CURRENT_QUES].userInput = 1
+        if (evt.target === option3) questions[CURRENT_QUES].userInput = 2
+        if (evt.target === option4) questions[CURRENT_QUES].userInput = 3
         removeListenersFromOptions()
 
         setTimeout(showAnswer, 1000)
@@ -236,9 +296,9 @@ function main(){
         next.className = 'button'
 
         explanationTitle.textContent = 'Explanation'
-        explanation.textContent = questions[CURRENT_QUES-1].explanation
+        explanation.textContent = questions[CURRENT_QUES].explanation
 
-        if (CURRENT_QUES == NUMBER_OF_QUES) {
+        if (CURRENT_QUES == NUMBER_OF_QUES-1) {
             next.textContent = 'SHOW RESULT'
             next.addEventListener('click', showResult)
         }
@@ -267,7 +327,11 @@ function main(){
         restart.className = 'button'
 
         resultTitle.textContent = 'Results'
-        result.textContent = 'You have scored '+score+' out of '+NUMBER_OF_QUES+'.'
+
+        if (score > 9) result.textContent = 'Well done ' + PLAYER_NAME + '! You have scored ' + score + ' out of ' + NUMBER_OF_QUES + ' which is an amazing score. Keep it up!'
+        else if (score < 5) result.textContent = 'Keep leaning ' + PLAYER_NAME + '! You have scored ' + score + ' out of ' + NUMBER_OF_QUES + ' but we know you can do better than this. We believe in you.'
+        else result.textContent = 'Congo ' + PLAYER_NAME +'! You have scored '+score+' out of '+NUMBER_OF_QUES+' which is a decent score. Keep working!'
+
         restart.textContent = 'REPLAY GAME'
         restart.addEventListener('click', () => {
             location.reload()
@@ -279,6 +343,32 @@ function main(){
 
         openNotifier()
     }
+
+    rightArrow.addEventListener('click', () => {
+        if(CURRENT_QUES == 14) {
+            alert('No more questions ahead')
+            return
+        }
+        else {
+            CURRENT_QUES++
+            // console.log(CURRENT_QUES)
+            addQuestion()
+
+        }
+    })
+
+    leftArrow.addEventListener('click', () => {
+        if (CURRENT_QUES == 0) {
+            alert('This is the very first question, we don\'t start counting from 0.')
+            return
+        }
+        else {
+            CURRENT_QUES--
+            // console.log(CURRENT_QUES)
+            addQuestion()
+
+        }
+    })
 }
 
 main()
